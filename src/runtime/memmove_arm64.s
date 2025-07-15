640391c0c5ff57acd2c9260abe4308e6b830a594
Xinliang Liu <xinliang.liu@linaro.org>
2025-07-15 06:59:30 +0000

    runtime: change to SIMD memmove
    Adapted from the original at:
    https://github.com/ARM-software/optimized-routines/blob/41e5ae54ce6eac3411cd7c51eb40068fbf187601/string/aarch64/memcpy-advsimd.S
    
    Signed-off-by: Xinliang Liu <xinliang.liu@linaro.org>

--- src/runtime/memmove_arm64.s
+++ src/runtime/memmove_arm64.s
@@ -1,4 +1,4 @@
-// Copyright 2014 The Go Authors. All rights reserved.
+// Copyright 2014-2025 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -7,232 +7,186 @@
 // See memmove Go doc for important implementation constraints.
 
 // Register map
-//
-// dstin  R0
-// src    R1
-// count  R2
-// dst    R3 (same as R0, but gets modified in unaligned cases)
-// srcend R4
-// dstend R5
-// data   R6-R17
-// tmp1   R14
+#define dstin	R0
+#define src	R1
+#define count	R2
+#define dst	R3
+#define srcend	R4
+#define dstend	R5
+#define A_l	R6
+#define A_h	R7
+#define B_l	R8
+#define B_h	R9
+#define C_l	R10
+#define tmp1	R14
 
+#define A_q	F0
+#define B_q	F1
+#define C_q	F2
+#define D_q	F3
+#define E_q	F4
+#define F_q	F5
+#define G_q	F6
+#define H_q	F7
+
+// This implementation handles overlaps and supports both memcpy and memmove
+// from a single entry point.  It uses unaligned accesses and branchless
+// sequences to keep the code small, simple and improve performance.
+//
 // Copies are split into 3 main cases: small copies of up to 32 bytes, medium
 // copies of up to 128 bytes, and large copies. The overhead of the overlap
 // check is negligible since it is only required for large copies.
 //
 // Large copies use a software pipelined loop processing 64 bytes per iteration.
-// The destination pointer is 16-byte aligned to minimize unaligned accesses.
+// The source pointer is 16-byte aligned to minimize unaligned accesses.
 // The loop tail is handled by always copying 64 bytes from the end.
 
 // func memmove(to, from unsafe.Pointer, n uintptr)
 TEXT runtime·memmove<ABIInternal>(SB), NOSPLIT|NOFRAME, $0-24
-	CBZ	R2, copy0
-
-	// Small copies: 1..16 bytes
-	CMP	$16, R2
-	BLE	copy16
-
-	// Large copies
-	CMP	$128, R2
+	ADD	count, src, srcend
+	CMP	$128, count
 	BHI	copy_long
-	CMP	$32, R2
+	ADD	count, dstin, dstend
+	CMP	$32, count
 	BHI	copy32_128
+	NOOP
 
-	// Small copies: 17..32 bytes.
-	LDP	(R1), (R6, R7)
-	ADD	R1, R2, R4          // R4 points just past the last source byte
-	LDP	-16(R4), (R12, R13)
-	STP	(R6, R7), (R0)
-	ADD	R0, R2, R5          // R5 points just past the last destination byte
-	STP	(R12, R13), -16(R5)
+	// Small copies: 0..32 bytes
+	CMP	$16, count
+	BLO	copy16
+	FMOVQ	(src), A_q
+	FMOVQ	-16(srcend), B_q
+	FMOVQ	A_q, (dstin)
+	FMOVQ	B_q, -16(dstend)
 	RET
 
-// Small copies: 1..16 bytes.
-copy16:
-	ADD	R1, R2, R4 // R4 points just past the last source byte
-	ADD	R0, R2, R5 // R5 points just past the last destination byte
-	CMP	$8, R2
-	BLT	copy7
-	MOVD	(R1), R6
-	MOVD	-8(R4), R7
-	MOVD	R6, (R0)
-	MOVD	R7, -8(R5)
-	RET
-
-copy7:
-	TBZ	$2, R2, copy3
-	MOVWU	(R1), R6
-	MOVWU	-4(R4), R7
-	MOVW	R6, (R0)
-	MOVW	R7, -4(R5)
-	RET
-
-copy3:
-	TBZ	$1, R2, copy1
-	MOVHU	(R1), R6
-	MOVHU	-2(R4), R7
-	MOVH	R6, (R0)
-	MOVH	R7, -2(R5)
-	RET
-
-copy1:
-	MOVBU	(R1), R6
-	MOVB	R6, (R0)
-
-copy0:
-	RET
-
-	// Medium copies: 33..128 bytes.
+	// Medium copies: 33..128 bytes
 copy32_128:
-	ADD	R1, R2, R4          // R4 points just past the last source byte
-	ADD	R0, R2, R5          // R5 points just past the last destination byte
-	LDP	(R1), (R6, R7)
-	LDP	16(R1), (R8, R9)
-	LDP	-32(R4), (R10, R11)
-	LDP	-16(R4), (R12, R13)
-	CMP	$64, R2
+	FLDPQ	(src), (A_q, B_q)
+	FLDPQ	-32(srcend), (C_q, D_q)
+	CMP	$64, count
 	BHI	copy128
-	STP	(R6, R7), (R0)
-	STP	(R8, R9), 16(R0)
-	STP	(R10, R11), -32(R5)
-	STP	(R12, R13), -16(R5)
+	FSTPQ	(A_q, B_q), (dstin)
+	FSTPQ	(C_q, D_q), -32(dstend)
 	RET
 
-	// Copy 65..128 bytes.
-copy128:
-	LDP	32(R1), (R14, R15)
-	LDP	48(R1), (R16, R17)
-	CMP	$96, R2
-	BLS	copy96
-	LDP	-64(R4), (R2, R3)
-	LDP	-48(R4), (R1, R4)
-	STP	(R2, R3), -64(R5)
-	STP	(R1, R4), -48(R5)
+	// Copy 8-15 bytes
+copy16:
+	TBZ	$3, count, copy8
+	MOVD	(src), A_l
+	MOVD	-8(srcend), A_h
+	MOVD	A_l, (dstin)
+	MOVD	A_h, -8(dstend)
+	RET
 
+	// Copy 4-7 bytes
+copy8:
+	TBZ	$2, count, copy4
+	MOVWU	(src), A_l
+	MOVWU	-4(srcend), B_l
+	MOVW	A_l, (dstin)
+	MOVW	B_l, -4(dstend)
+	RET
+
+	// Copy 65..128 bytes
+copy128:
+	FLDPQ	32(src), (E_q, F_q)
+	CMP	$96, count
+	BLS	copy96
+	FLDPQ	-64(srcend), (G_q, H_q)
+	FSTPQ	(G_q, H_q), -64(dstend)
 copy96:
-	STP	(R6, R7), (R0)
-	STP	(R8, R9), 16(R0)
-	STP	(R14, R15), 32(R0)
-	STP	(R16, R17), 48(R0)
-	STP	(R10, R11), -32(R5)
-	STP	(R12, R13), -16(R5)
+	FSTPQ	(A_q, B_q), (dstin)
+	FSTPQ	(E_q, F_q), 32(dstin)
+	FSTPQ	(C_q, D_q), -32(dstend)
+	RET
+
+	// Copy 0..3 bytes using a branchless sequence.
+copy4:
+	CBZ	count, copy0
+	LSR	$1, count, tmp1
+	MOVBU	(src), A_l
+	MOVBU	-1(srcend), C_l
+	MOVBU	(src)(tmp1), B_l
+	MOVB	A_l, (dstin)
+	MOVB	B_l, (dstin)(tmp1)
+	MOVB	C_l, -1(dstend)
+copy0:
 	RET
 
 	// Copy more than 128 bytes.
 copy_long:
-	ADD	R1, R2, R4 // R4 points just past the last source byte
-	ADD	R0, R2, R5 // R5 points just past the last destination byte
-	MOVD	ZR, R7
-	MOVD	ZR, R8
+	ADD	count, dstin, dstend
 
-	CMP	$1024, R2
-	BLT	backward_check
-	// feature detect to decide how to align
-	MOVBU	runtime·arm64UseAlignedLoads(SB), R6
-	CBNZ	R6, use_aligned_loads
-	MOVD	R0, R7
-	MOVD	R5, R8
-	B	backward_check
-use_aligned_loads:
-	MOVD	R1, R7
-	MOVD	R4, R8
-	// R7 and R8 are used here for the realignment calculation. In
-	// the use_aligned_loads case, R7 is the src pointer and R8 is
-	// srcend pointer, which is used in the backward copy case.
-	// When doing aligned stores, R7 is the dst pointer and R8 is
-	// the dstend pointer.
+	// Use backwards copy if there is an overlap.
+	SUB	src, dstin, tmp1
+	CMP	count, tmp1
+	BLO	copy_long_backwards
 
-backward_check:
-	// Use backward copy if there is an overlap.
-	SUB	R1, R0, R14
-	CBZ	R14, copy0
-	CMP	R2, R14
-	BCC	copy_long_backward
-
-	// Copy 16 bytes and then align src (R1) or dst (R0) to 16-byte alignment.
-	LDP	(R1), (R12, R13)     // Load  A
-	AND	$15, R7, R14         // Calculate the realignment offset
-	SUB	R14, R1, R1
-	SUB	R14, R0, R3          // move dst back same amount as src
-	ADD	R14, R2, R2
-	LDP	16(R1), (R6, R7)     // Load   B
-	STP	(R12, R13), (R0)     // Store A
-	LDP	32(R1), (R8, R9)     // Load    C
-	LDP	48(R1), (R10, R11)   // Load     D
-	LDP.W	64(R1), (R12, R13)   // Load      E
-	// 80 bytes have been loaded; if less than 80+64 bytes remain, copy from the end
-	SUBS	$144, R2, R2
+	// Copy 16 bytes and then align src to 16-byte alignment.
+	FMOVQ	(src), D_q
+	AND	$15, src, tmp1
+	BIC	$15, src, src
+	SUB	tmp1, dstin, dst
+	ADD	tmp1, count, count  // count is now 16 too large.
+	FLDPQ	16(src), (A_q, B_q)
+	FMOVQ	D_q, (dstin)
+	FLDPQ	48(src), (C_q, D_q)
+	SUBS	$(128+16), count, count  // Test and readjust count.
 	BLS	copy64_from_end
 
 loop64:
-	STP	(R6, R7), 16(R3)     // Store  B
-	LDP	16(R1), (R6, R7)     // Load   B (next iteration)
-	STP	(R8, R9), 32(R3)     // Store   C
-	LDP	32(R1), (R8, R9)     // Load    C
-	STP	(R10, R11), 48(R3)   // Store    D
-	LDP	48(R1), (R10, R11)   // Load     D
-	STP.W	(R12, R13), 64(R3)   // Store     E
-	LDP.W	64(R1), (R12, R13)   // Load      E
-	SUBS	$64, R2, R2
+	FSTPQ	(A_q, B_q), 16(dst)
+	FLDPQ	80(src), (A_q, B_q)
+	FSTPQ	(C_q, D_q), 48(dst)
+	FLDPQ	112(src), (C_q, D_q)
+	ADD	$64, src, src
+	ADD	$64, dst, dst
+	SUBS	$64, count, count
 	BHI	loop64
 
 	// Write the last iteration and copy 64 bytes from the end.
 copy64_from_end:
-	LDP	-64(R4), (R14, R15)  // Load       F
-	STP	(R6, R7), 16(R3)     // Store  B
-	LDP	-48(R4), (R6, R7)    // Load        G
-	STP	(R8, R9), 32(R3)     // Store   C
-	LDP	-32(R4), (R8, R9)    // Load         H
-	STP	(R10, R11), 48(R3)   // Store    D
-	LDP	-16(R4), (R10, R11)  // Load          I
-	STP	(R12, R13), 64(R3)   // Store     E
-	STP	(R14, R15), -64(R5)  // Store      F
-	STP	(R6, R7), -48(R5)    // Store       G
-	STP	(R8, R9), -32(R5)    // Store        H
-	STP	(R10, R11), -16(R5)  // Store         I
+	FLDPQ	-64(srcend), (E_q, F_q)
+	FSTPQ	(A_q, B_q), 16(dst)
+	FLDPQ	-32(srcend), (A_q, B_q)
+	FSTPQ	(C_q, D_q), 48(dst)
+	FSTPQ	(E_q, F_q), -64(dstend)
+	FSTPQ	(A_q, B_q), -32(dstend)
 	RET
 
-	// Large backward copy for overlapping copies.
-	// Copy 16 bytes and then align srcend (R4) or dstend (R5) to 16-byte alignment.
-copy_long_backward:
-	LDP	-16(R4), (R12, R13)
-	AND	$15, R8, R14
-	SUB	R14, R4, R4
-	SUB	R14, R2, R2
-	LDP	-16(R4), (R6, R7)
-	STP	(R12, R13), -16(R5)
-	LDP	-32(R4), (R8, R9)
-	LDP	-48(R4), (R10, R11)
-	LDP.W	-64(R4), (R12, R13)
-	SUB	R14, R5, R5
-	SUBS	$128, R2, R2
+	// Large backwards copy for overlapping copies.
+	// Copy 16 bytes and then align srcend to 16-byte alignment.
+copy_long_backwards:
+	CBZ	tmp1, copy0
+	FMOVQ	-16(srcend), D_q
+	AND	$15, srcend, tmp1
+	BIC	$15, srcend, srcend
+	SUB	tmp1, count, count
+	FLDPQ	-32(srcend), (A_q, B_q)
+	FMOVQ	D_q, -16(dstend)
+	FLDPQ	-64(srcend), (C_q, D_q)
+	SUB	tmp1, dstend, dstend
+	SUBS	$128, count, count
 	BLS	copy64_from_start
 
-loop64_backward:
-	STP	(R6, R7), -16(R5)
-	LDP	-16(R4), (R6, R7)
-	STP	(R8, R9), -32(R5)
-	LDP	-32(R4), (R8, R9)
-	STP	(R10, R11), -48(R5)
-	LDP	-48(R4), (R10, R11)
-	STP.W	(R12, R13), -64(R5)
-	LDP.W	-64(R4), (R12, R13)
-	SUBS	$64, R2, R2
-	BHI	loop64_backward
+loop64_backwards:
+	FMOVQ	B_q, -16(dstend)
+	FMOVQ	A_q, -32(dstend)
+	FLDPQ	-96(srcend), (A_q, B_q)
+	FMOVQ	D_q, -48(dstend)
+	FMOVQ.W	C_q, -64(dstend)
+	FLDPQ	-128(srcend), (C_q, D_q)
+	SUB	$64, srcend, srcend
+	SUBS	$64, count, count
+	BHI	loop64_backwards
 
-	// Write the last iteration and copy 64 bytes from the start.
+	// Write the last iteration and copy 64 bytes from the start
 copy64_from_start:
-	LDP	48(R1), (R2, R3)
-	STP	(R6, R7), -16(R5)
-	LDP	32(R1), (R6, R7)
-	STP	(R8, R9), -32(R5)
-	LDP	16(R1), (R8, R9)
-	STP	(R10, R11), -48(R5)
-	LDP	(R1), (R10, R11)
-	STP	(R12, R13), -64(R5)
-	STP	(R2, R3), 48(R0)
-	STP	(R6, R7), 32(R0)
-	STP	(R8, R9), 16(R0)
-	STP	(R10, R11), (R0)
+	FLDPQ	32(src), (E_q, F_q)
+	FSTPQ	(A_q, B_q), -32(dstend)
+	FLDPQ	(src), (A_q, B_q)
+	FSTPQ	(C_q, D_q), -64(dstend)
+	FSTPQ	(E_q, F_q), 32(dstin)
+	FSTPQ	(A_q, B_q), (dstin)
 	RET
